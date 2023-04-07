fa98f234ee223fb24b9f401a9fe28d8b647f0d33
chaitnya23 <chaitnyagiri2306@gmail.com>
2023-04-07 13:12:58 +0530

    improved responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar({onHome , changeToHome }) {
     return (
 
-        <div style={{ display: "flex", padding: "5px 4rem 0 4rem", justifyContent: "space-between" }}>
+        <div className='nav' style={{ display: "flex", padding: "5px 4rem 0 4rem", justifyContent: "space-between" ,position:"sticky" ,top:"0px" }}>
             <div  style={{fontWeight:`${onHome?"bold":"400"}` ,cursor:"pointer"}} onClick={()=>changeToHome(true)}>Home</div>
             <div style={{fontWeight:`${!onHome?"bold":"400"}` ,cursor:"pointer"}} onClick={()=>changeToHome(false)}>Bookmarks</div>
         </div>
     )
-}
+} 
